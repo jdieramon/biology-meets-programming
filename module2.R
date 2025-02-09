f34b7569c43ddd44803e2be23e0902fc5ca12eb7
Jose V. Die <jodiera@upv.es>
2025-02-09 12:05:38 +0100

    Create module2.R

---
+++ module2.R
@@ -0,0 +1,55 @@
+fasterSymbolArray <- function(text, pattern) {
+  coords = list()
+  n = nchar(text)
+  extended_genome = paste0(text, substring(text, 1, floor(n/2))) 
+  
+  # look at the first half of Genome to compute first array value
+  coords[1] = patternCount(substring(text, 1, floor(n/2)), pattern)
+  for(i in 2:n) {
+    # the current array value can differ from the previous array value by at most 1
+    if(substring(extended_genome, i-1, i-1) == pattern) {
+      coords[i] = coords[[i-1]] - 1 }
+    if(substring(extended_genome, i + floor(n/2) - 1, i + floor(n/2) - 1) == pattern) {
+      coords[i] = coords[[i-1]] + 1
+    }
+  }
+  
+  coords
+  
+}
+
+pattern = "A"
+text = "AAAAGGGG"
+fasterSymbolArray(text, pattern)
+
+
+skew_diagram <- function(text, pattern) {
+  
+  plot(unlist(fasterSymbolArray(text, pattern)), 
+       ylab = paste("count of", pattern, "in half-genome starting at given position"), 
+       xlab = "genome position")
+}
+
+skew_diagram(text, pattern)
+
+
+#url <- "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/E_coli.txt"
+#ecoli <- readLines(url)
+#head(ecoli)  # View first few lines
+
+#skew_diagram(ecoli, "C")
+
+
+
+genome = "CATGGGCATCGGCCATACGCC"
+
+count = 0
+s = 1
+l = str_length(pattern)
+
+while (l <= str_length(text)) {
+  if(str_sub(text, s, l) == pattern) {
+    count = count + 1 }
+
+
+
